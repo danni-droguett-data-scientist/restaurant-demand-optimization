dd0aabb39e66b92774ef113b0916f2885e43489b
danni-droguett-data-scientist <dannidro@gmail.com>
2026-02-27 19:26:23 -0300

    Add synthetic dataset generator notebook and main entrypoint

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# src/main.py
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+print("Entorno listo. Librerías importadas correctamente.")
